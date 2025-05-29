527d1c62dbe65b759f45bd14296d1b12ba7e00f3
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-29 14:52:02 +0530

    jpa example

---
+++ student-hibernate-jpa-example/src/main/java/com/shreya/jpa/StudentHibernateJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHibernateJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHibernateJpaExampleApplication.class, args);
+	}
+
+}
